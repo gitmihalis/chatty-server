167970f88f24ae8cdb671f1c7b86d026e7920942
gitmihalis <mihalisfthenos@icloud.com>
2017-07-28 01:58:11 +0000

    drop cliend Ids and send number of connected clients to the client instead

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // Server
-// Server
 const express = require('express')
 const SockectServer = require('ws').Server
 
@@ -16,34 +15,39 @@ const server = express()
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`))
 // Create a web sockets server
 const wss = new SockectServer({ server })
-	// Set up a callback that will run when a client connects to the server
-	// When a client connects they are assigned a socket, represented by
-	// the ws parameter in the callback.
+
+// TODO Currently connected clients
+// const clients = {}
+
+// A callback that will run when a client connects to the server
+// When a client connects they are assigned a socket, represented by
+// the ws parameter in the callback.
 wss.on('connection', (client) => {
-	console.log('Client connected')
-		 // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  client.on('close', () => console.log('Client disconnected'))
+	clientConnected()
+	//  A callback for when a client closes the socket. This usually means they closed their browser.
+  client.on('close', () => clientDisconnected() )
 
   client.on('message', (incoming) => {
  	  console.log('incoming message: ', incoming)
+
  		const data = JSON.parse(incoming)
+
  		switch(data.type) {
  			case "postMessage":
  				data.id = uuid()
  				data.type = 'incomingMessage'
  				wss.broadcast(JSON.stringify(data))
- 				break;
+ 				break
  			case "postNotification":
  				data.id = uuid()
  				data.type = 'incomingNotification'
  				wss.broadcast(JSON.stringify(data))
- 				break;
+ 				break
  			default:
  				console.error('Unkown data type ', data.type)
  		}
-	})
+ 	})
 })
-
 // Broadcast - Goes through each client and sends message data
 wss.broadcast = function(data) {
   wss.clients.forEach(function(client) {
@@ -51,6 +55,29 @@ wss.broadcast = function(data) {
       client.send(data)
     }
   })
+}
+// A client is added to clients on a connection event
+function clientConnected() {
+  // Setup message to be set to the client
+  // Includes all currently connected clients
+  const connectionMsg = {
+    type: 'connectionNotification',
+    id: uuid(),
+    clients: wss.clients.length,
+  }
+  wss.broadcast(JSON.stringify(connectionMsg))
+  console.log(`>> client connected`)
+}
+
+// Disconnection event
+function clientDisconnected() {
+	const disconnectionMsg = {
+		type: 'disconnectNotification',
+		id: uuid(),
+		clients: wss.clients.length
+		}
+  wss.broadcast(JSON.stringify(disconnectionMsg))
+  console.log(`<< client disconnected`)
 }
 
 
